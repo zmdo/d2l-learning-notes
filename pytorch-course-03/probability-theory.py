dcab66be0c2ba5353a11f34fb92977b074a4ac4e
zmdo <i@zmdo.cn>
2024-06-21 00:37:00 +0800

    记录第三课笔记

--- pytorch-course-03/probability-theory.py
+++ pytorch-course-03/probability-theory.py
@@ -3,3 +3,8 @@ import torch
 # multinomial 多项式
 from torch.distributions import multinomial
 
+# 这里模拟的是一个投骰子的的概率，即每个面上的概率为 1/6
+fair_probs = torch.ones(6,dtype=torch.float32)/6
+# 这里是采样过程，在投骰子的概率分布上采10次样， sample 采样
+result = multinomial.Multinomial(10,fair_probs).sample()
+print(result)
